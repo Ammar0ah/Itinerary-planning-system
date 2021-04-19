3d302b19505ae5006a0eb99f0bb56694c74d8728
osamaalhalabi <eng.osama1998@gmail.com>
2021-04-19 17:07:07 +0300

    updating trips data collector

--- Search_engine.py
+++ Search_engine.py
@@ -8,35 +8,87 @@ class Search_Engine:
         self.hotels = Hotels()
         self.places = Places()
     
-    class engine_types(Enum):
+    class EngineType(Enum):
         HOTELS = 1,
         PLACES = 2,
         
-    class hse_end_points(Enum):
+    class HSE_EndPoints(Enum):
         BOOK = 1, #location_name & other booking information 'dict'
         DETAILS = 2, #hotel_id 'int'
         IMAGES = 3,   #hotel_id 'int'
         REVIEWS = 4,  #hotel_id 'int'
-        HOTEL = 5,    #hotel_name 'str'
+        FINDHOTEL = 5,    #hotel_name 'str'
         BESTCHOISE = 6, # location 'str
 
-    class pse_end_points(Enum):
+    class PSE_EndPoints(Enum):
         DETAILS = 1, #place_id : str
         LOCATION = 2, #place_name & wanted_filters
         LISTVIEW = 3, #place_name & wanted_filters
         COORDINATES = 4, #lat & lon : str, kind : str 
         BESTCHOISE = 5, #lat & lon : str, places count : int, kind : str 
-    def collect_trip_components(self, location : str, num_of_days : int):
-        trip_components = []
-        starting_pt = self.hotels.best_hotels_in_country(location) #starting point -> first hotel.
-        fortification = self.places.get_top_rated_places(starting_pt['0']['coordinate']['lat'],
-                                                       starting_pt['0']['coordinate']['lon'],
-                                                       int(num_of_days * 2.6),
-                                                      'fortifications')
-        trip_components.append(Item('hotel', starting_pt['0']))
-        for fortification in fortification:
-            trip_components.append(Item('fortification', fortification))  
-        return trip_components
+        
+    class OptionalPlacesKinds(Enum):
+        HIS = 'historic',
+        CUL = 'cultural',
+        NAT = 'natural',
+        REL = 'religion',
+        SPO = 'sport',
+        ARC = 'architecture',
+    
+    class TirpMode(Enum):
+        extended_trip = 1,
+        focused_trip = 2,
+
+        
+    ### need to specify the calculation mechanism of items count per trip.... -> dist
+    def collect_trip_components(self, location : str, num_of_days : int, preferences : list, trip_mode : str):
+        if bool(preferences):
+            if trip_mode not in self.TirpMode.__members__:
+                raise ValueError('You have entered wrong mode!')
+            else:
+                trip_components = []
+                fixed_places = ['fast_food', 'malls', 'marketplaces', 'restaurants', 'cafes'] 
+                
+                starting_pt = self.hotels.best_hotels_in_country(location) #starting point -> first hotel.
+                trip_components.append(Item('hotel', starting_pt['0']))
+                
+                dist = 0
+                suspicion = 0.2
+                if trip_mode == 'focused_trip':
+                    dist = num_of_days + 0.5
+                    
+                elif trip_mode == 'extended_trip':
+                    dist = num_of_days * 1.5
+                    
+                for kind in fixed_places:
+                    suspicion = suspicion * -1.1
+                    dist = dist + suspicion
+                    print('collecting', kind, '...')
+                    places = self.places.get_top_rated_places(starting_pt['0']['coordinate']['lat'],
+                                                               starting_pt['0']['coordinate']['lon'],
+                                                               round(dist),
+                                                               kind)
+
+                    for place in places:
+                        trip_components.append(Item(kind, place))
+                   
+                for kind in preferences:
+                    suspicion = suspicion * -1.1
+                    dist = dist + suspicion
+                    print('collecting', str(self.OptionalPlacesKinds[kind].value[0]), '...')
+                    places = self.places.get_top_rated_places(starting_pt['0']['coordinate']['lat'],
+                                                               starting_pt['0']['coordinate']['lon'],
+                                                               round(dist),
+                                                               self.OptionalPlacesKinds[kind].value[0])
+
+                    for place in places:
+                        trip_components.append(Item(str(self.OptionalPlacesKinds[kind].value[0]), place))
+                
+                
+                return set(trip_components)
+        else: 
+            raise ValueError('cannot collect trip components without specifying kinds!')  
+            
     def get(self, engine_type, end_point ,query_params):
 
         def hotel_search_engine_endpoints_es(end_point, query_params):
@@ -47,7 +99,7 @@ class Search_Engine:
                 'check_in_date',
                 'check_out_date',
             ]
-            if end_point not in self.hse_end_points.__members__:
+            if end_point not in self.HSE_EndPoints.__members__:
                 raise ValueError('fuck A very specific bad thing happened.')
             else:    
                 if end_point == 'DETAILS':
@@ -60,7 +112,7 @@ class Search_Engine:
                         return self.hotels.get_hotel_imgs(query_params['id'])
                     else:
                         raise ValueError('A very specific bad thing happened.')
-                elif end_point == 'HOTEL':      
+                elif end_point == 'FINDHOTEL':      
                     if 'name' in query_params:
                         return self.hotels.get_hotel_details_by_name(query_params['name'])
                     else:
@@ -88,7 +140,7 @@ class Search_Engine:
                     else:
                         raise ValueError('A very specific bad thing happened.')           
         def place_search_engine_endpoints_es(end_point, query_params):
-            if end_point not in self.pse_end_points.__members__:
+            if end_point not in self.PSE_EndPoints.__members__:
                 raise ValueError('Can not Find the Entered Endpoint!, try something else')
             else:    
                 if end_point == 'DETAILS':
